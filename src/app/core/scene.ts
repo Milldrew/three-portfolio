8db69d3489126429362d1b4575543086a3332a48
Andrew Miller <Andrew.patrick.miller.2021@gmail.com>
2023-11-11 12:15:44 -0700

    📅  | Sat Nov 11 12:15:44 MST 2023

--- src/app/core/scene.ts
+++ src/app/core/scene.ts
@@ -12,9 +12,8 @@ export class CustomScene {
     const canvas = document.querySelector('canvas#scene') as HTMLCanvasElement
     CustomScene.renderer = new THREE.WebGLRenderer({canvas})
     CustomScene.renderer.setSize(window.innerWidth, window.innerHeight)
-    CustomScene.camera.position.set(2, 2, 8)
-
-    const floor = Walls.createAWall(0x00ff00, 1, 0, 1, 1, 1, 1, CustomScene.scene)
+    CustomScene.camera.position.set(0, 10, 0)
+    const floor = Walls.createAWall(0xadadad, 4, 3, 0, 4, .04, 5, CustomScene.scene)
     //point camera at floor
     CustomScene.camera.lookAt(floor.position)
     CustomScene.scene.add(new THREE.AxesHelper)
